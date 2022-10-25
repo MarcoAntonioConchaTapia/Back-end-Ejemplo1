95d06f3d814c6d68d6e4281ac58c0c0361b680d4
Marco <marco.ncr05@gmail.com>
2022-10-24 20:01:03 -0500

    Agregar archivos de proyecto.

---
+++ Controllers/CalculosController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class CalculosController : Controller
+    {
+        // GET: Calculos
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Suma(string n1, string n2)
+        {
+            int resultado = 0;
+            resultado = Convert.ToInt16(n1) + Convert.ToInt16(n2);
+            ViewBag.res = resultado;
+
+            return View();
+        }
+        public ActionResult Operacion()
+        {
+            OperasBas op = new OperasBas();
+            return View(op);
+        }
+        [HttpPost]
+
+
+        public ActionResult Operacion(OperasBas op)
+        {
+            op.Suma();
+            return View(op);
+        }
+
+        public ActionResult Resta(OperasBas op)
+        {
+            op.Resta();
+            return View(op);
+        }
+    }
+}
---
+++ Models/OperasBas.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class OperasBas
+    {
+        public int Num1 { get; set; }
+
+        public int Num2 { get; set; }
+
+        public int res { get; set; }
+
+        public void Suma()
+        {
+            this.res = this.Num1 + this.Num2;
+        }
+
+        public void Resta()
+        {
+            this.res = this.Num1 - this.Num2;
+        }
+
+    }
+}
